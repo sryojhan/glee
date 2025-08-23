562ec75db7546df4f20f86fee3d8012f57fd3f82
Yojhan <sryojhan@gmail.com>
2025-08-23 19:20:34 +0200

    Utilts class. Closes #36

---
+++ core/Utils.cs
@@ -0,0 +1,11 @@
+namespace Glee;
+
+
+public static class Utils
+{
+    public static Vector2 Left => new(-1, 0);
+    public static Vector2 Right => new(1, 0);
+    public static Vector2 Down => new(0, -1);
+    public static Vector2 Up => new(0, 1);
+
+}
