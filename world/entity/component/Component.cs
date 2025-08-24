4c6a9b924acbe28f722176efa95783293a414fb5
Yojhan <sryojhan@gmail.com>
2025-08-24 03:02:12 +0200

    Component get component shortcut

--- world/entity/component/Component.cs
+++ world/entity/component/Component.cs
@@ -10,4 +10,7 @@ public abstract class Component : GleeObject
     public World world => entity.world;
     public Time Time => world.Time;
     public bool Enabled { get; set; } = true;
+
+
+    public ComponentType GetComponent<ComponentType>() where ComponentType : Component => entity.GetComponent<ComponentType>();
 }
